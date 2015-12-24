d43d42d66e8acf22da996d53fa7047ba3f5aed01
Monro, Ben <Ben.Monro@concur.com>
2015-12-24 00:58:53 -0800

    simple prompt version working

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
-
+import {values, keys} from 'lodash';
 import program from 'commander';
 import replace from 'replace';
 import {find} from 'find-in-files';
+import {prompt} from 'prompt-sync';
+import S from 'string';
 
 import {init as initSkeleton} from 'init-skeleton';
 const defaultSkeleton = 'https://github.com/bare-bones/scalp-simple';
@@ -11,13 +13,35 @@ const defaultSkeleton = 'https://github.com/bare-bones/scalp-simple';
 program
     .version(require('../package.json').version)
     .command('new [path]')
-   // .option('-s, --skeleton [name]', 'skeleton name or URL from brunch.io/skeletons')
+    // .option('-s, --skeleton [name]', 'skeleton name or URL from brunch.io/skeletons')
     .action((rootPath, options) => {
 
         const skeleton = options.skeleton || process.env.BRUNCH_INIT_SKELETON || defaultSkeleton;
         initSkeleton(skeleton, {rootPath}, err => {
-            find('scalp-(\\w+)', rootPath).then(results => {
-                console.log(results);
+            let matches = {};
+            find('scalp_(\\w+)', rootPath).then(results => {
+                values(results).forEach(result => {
+                    result.matches.forEach(match => {
+                        let key = match.replace('scalp_', '');
+                        matches[key] = {prompt: S(key).humanize().s}
+                    })
+                });
+                console.log(matches);
+
+                for(let key in matches) {
+                    let variableInfo = matches[key];
+                    console.log(variableInfo.prompt);
+                    matches[key].value = prompt();
+                    replace({
+                        regex:`scalp_${key}`,
+                        replacement:matches[key].value,
+                        paths:[rootPath],
+                        recursive:true,
+                        silent:true
+                    });
+                }
+
+
             });
 
         });
